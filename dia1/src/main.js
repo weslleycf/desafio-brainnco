b04a38f2eb9d2addc23b8ada29af3bc94a314f62
Weslley do Cati Ferreira <weslley.docati@gmail.com>
2021-08-17 14:54:51 -0300

    Solução do exercício 04

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -4,3 +4,18 @@ document.querySelector('[data-js="app"]').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+
+const link = document.querySelector('[data-js="link"]');
+link.addEventListener('click', () => {
+  const app = document.querySelector('[data-js="app"]'); 
+  if (app.getAttribute("style") === "visibility: hidden"){
+    app.setAttribute("style", "visibility: visible");
+    link.innerHTML = "Esconder";
+  } else {
+    app.setAttribute("style", "visibility: hidden");
+    link.innerHTML = "Exibir";
+  }
+   
+}
+, false );
+
